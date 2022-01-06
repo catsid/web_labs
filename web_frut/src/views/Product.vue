53ec34789de44a30ec173f49dad104b1e4c3dc4d
catsid <97018991+catsid@users.noreply.github.com>
2022-01-06 17:31:17 +0300

    Update Product.vue

--- web_frut/src/views/Product.vue
+++ web_frut/src/views/Product.vue
@@ -1,30 +1,30 @@
 <template>
-  <section class="product_page">
-    <div v-for="item in product_card" class="projects-item">
-      <Product_card :product_card="item"></Product_card>
-    </div>
-  </section>
+    <section class="product_page">
+      <div v-for="item in product_page " class="projects-item">
+        <Product_page :product_page ="item"></Product_page>
+      </div>
+    </section>
 </template>
 
 <script>
-import Product_card from "../components/Product_card";
+import Product_page from "../components/Product_page ";
 
 export default {
   name: "Product",
-  components: {Product_card},
+  components: {Product_page},
   data() {
     return {
-      product_card: [
+      product_page: [
         {
           id: 1,
           name: 'Миндаль сырой(100 гр.)',
           country: 'Страна происхождения: Таджикистан'
         },
-        {
+	{
           id: 2,
           name: 'Финики королевские(100 гр.)',
-          country: 'Страна происхождения: Израиль'
-        },
+          country: 'Страна происхождения: Израиль'        
+	},
         {
           id: 3,
           name: 'Кешью сырой(100 гр.)',
